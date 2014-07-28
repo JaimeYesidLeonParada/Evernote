d4ec9803521811af4e0e4751297798c4753a1299
JaimeLeon <jaime.leon@koombea.com>
2014-07-27 20:29:28 -0500

    Initial Commit

---
+++ EverNote/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EverNote
+//
+//  Created by Yesid Leon on 27/07/14.
+//  Copyright (c) 2014 Yesid Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
